ae16170da3154f8958294f953aa44f12b90dfb41
Valeria Nedelko <galanthusweekly@gmail.com>
2022-03-04 00:59:30 +0300

    second commit

---
+++ 1/1/AboutViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AboutViewController.swift
+//  1
+//
+//  Created by Валерия Неделько on 15.01.22.
+//
+
+import UIKit
+
+    class AboutViewController: UIViewController {
+    @IBAction func close() {
+    dismiss(animated: true, completion: nil)
+    }
+}
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
---
+++ 1/1/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  1
+//
+//  Created by Валерия Неделько on 14.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
+    var currentValue: Int = 0
+    var targetValue = 0
+    var score = 0
+    var round = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        currentValue = lroundf(slider.value)
+        targetValue = Int.random(in: 1...100)
+        startNewGame()
+    }
+    
+    @IBAction func showAlert() {
+        let difference = abs(targetValue - currentValue)
+        var points = 100 - difference
+        let title: String
+        if difference == 0 {
+            title = "Perfect!"
+            points += 100
+        } else if difference < 5 {
+            title = "You almost had it!"
+            if difference == 1 {
+                points += 50 
+            }
+        } else if difference < 10 {
+            title = "Pretty good!"
+        } else {
+            title = "Not even close..."
+        }
+        score += points
+        let message = "You scored \(points) points"
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: { _ in self.startNewRound()})
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+    currentValue = lroundf(slider.value)
+    }
+    
+    @IBAction func startNewGame() {
+    score = 0
+    round = 0
+    startNewRound()
+    }
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        round += 1
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
+}
+
